bfe872d95321142a878d839c312920465240b140
Joshua Amira <joshua.amira@foxmoat.com>
2020-01-31 20:41:49 +0000

    merged csproj with master

--- Tailspin.SpaceGame.Web/Startup.cs
+++ Tailspin.SpaceGame.Web/Startup.cs
@@ -1,13 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-<<<<<<< HEAD
-using Microsoft.AspNetCore.HttpsPolicy;
-=======
->>>>>>> master
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
